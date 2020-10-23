bb641a495d462810b0ad5232ebadd60fd0c1d676
Mikah Kainen <mikah@mltd.com>
2020-10-23 14:25:57 -0700

    Did my homework

--- ClassIntro/Main.cpp
+++ ClassIntro/Main.cpp
@@ -2,6 +2,57 @@
 #include <string>
 
 
+class Person
+{
+private:
+	int age;
+	int height;
+	std::string name;
+
+public:
+	Person(int age, int height, std::string name) : age(age), height(height), name(name)
+	{
+
+	}
+
+	int GetAge()
+	{
+		return age;
+	}
+	void SetAge(int age)
+	{
+		this->age = age;
+	}
+
+	int GetHeight()
+	{
+		return height;
+	}
+	void SetHeight(int height)
+	{
+		this->height = height;
+	}
+
+	std::string GetName()
+	{
+		return name;
+	}
+	void SetName(std::string name)
+	{
+		this->name = name;
+	}
+
+	void Print()
+	{
+		std::cout << "name: " << name << ", height: " << height << ", age: " << age << std::endl;
+	}
+};
+
+//Person::Print()
+//{
+//
+//}
+
 class Test
 {
 
@@ -52,7 +103,8 @@ int main()
 	//Get Set for each
 	//Print function (name, age, height)
 
-
+	Person person{7, 8, "bob"};
+	person.Print();
 
 	//int x = 2;
 	//int y = 4;
@@ -60,11 +112,11 @@ int main()
 	//add(x, y, z);
 	//std::cout << z;
 
-	Test test{ 5 };
-	test.SetSomething(4);
-	std::cout << std::endl << test.GetSomething() << std::endl;
-	test.Print();
-	Test test2{};
-	test.Print();
+	//Test test{ 5 };
+	//test.SetSomething(4);
+	//std::cout << std::endl << test.GetSomething() << std::endl;
+	//test.Print();
+	//Test test2{};
+	//test.Print();
 	
 }
